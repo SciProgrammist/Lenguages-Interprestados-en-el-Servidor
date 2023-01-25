1a92b2ac04f3af1f7e011e212d3db04d427db937
SciProgrammist <RP191495@alumno.udb.edu.sv>
2023-01-25 00:01:20 -0600

    Guia01Completa

--- Guia1_RP191495/Ejercicio1/partehtmlphp.php
+++ Guia1_RP191495/Ejercicio1/partehtmlphp.php
@@ -3,7 +3,8 @@
 <head>
     <title>Scripts con (X) HTML y PHP</title>
     <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum=1.0" />
+    <meta name="viewport" content="width=device-width, user-scalable=no,
+    initial-scale=1.0, maximum-scale=1.0, minimum=1.0" />
     <!-- Definiendo una hoja de estilo local-->
     <link rel="stylesheet" media="screen" href="css/htmlphp.css">
     <script src="js/modernizr.custom.lis.js"></script>
---
+++ Guia1_RP191495/Ejercicio4/opera_comparacion.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Ejemplo de PHP - Operadores de comparacion</title>
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+<?PHP
+$a = 8;
+$b = 3;
+$c = 3;
+echo $a == $b, "<br>";
+echo $a != $b, "<br>";
+echo $a < $b, "<br>";
+echo $a > $b, "<br>";
+echo $a >= $c, "<br>";
+echo $b <= $c, "<br>";
+?>
+
+</body>
+</html>
